e0bc16712f68b9eb7a9ba17582c9aa55fa9003c0
MAHESH <mah.dot98@gmail.com>
2021-02-23 10:22:31 +0545

    Initial commit

---
+++ pythonworkshop/ratt.py
@@ -0,0 +1,5 @@
+#write a program that reads  length of the bases and height of the right angled triangle area
+height=int(input("enter the height of the triangle:"))
+base=int(input("enter the base of the triangle:"))
+area=1/2*base*height
+print("the area of right angled triangle is:",area)
