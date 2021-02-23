188565bb4a81680c96b356be73e5aa1e7716bf12
MAHESH <mah.dot98@gmail.com>
2021-02-23 09:36:31 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("enter the first value:",a)
+print("enter the second value:",b)
+print("enter the third value",c)
+d=a+b+c
+print("the sum is",d)
---
+++ pythonworkshop/day3.py
@@ -0,0 +1,6 @@
+# write a program that takes three number and print thier sum. Every number is given on a seperate line.
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+d=a+b+c
+print("the sum is:",d)
