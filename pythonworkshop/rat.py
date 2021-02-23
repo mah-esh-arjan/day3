597d6da14d02831932a571133c37ad8b09e627ec
MAHESH <mah.dot98@gmail.com>
2021-02-23 09:50:46 +0545

    Initial commit

--- pythonworkshop/rat.py
+++ pythonworkshop/rat.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-#Write
-=======
 #Write a program that reads the length of the bases and height of the right angled triangle and print the area
 #every number is given on a separate line.
 height=int(input("enter the height:"))
 base=int(input("enter the base:"))
 area=1/2*base*height
 print("the area of the right angled triangle is:",area)
->>>>>>> 5c082af (Initial commit)
+
