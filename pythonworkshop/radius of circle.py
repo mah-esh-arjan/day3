eab503feeb925a64508d35ece87cbc758edc2163
MAHESH <mah.dot98@gmail.com>
2021-03-16 10:07:34 +0545

    inital commit:

--- pythonworkshop/radius of circle.py
+++ pythonworkshop/radius of circle.py
@@ -1,4 +1,4 @@
 #write a python program which accepts the radius of a circle form the user and compute the area(area of circle = Pi *r*r)
-radius = int(input("enter the radius of the circle: "))
-area_circle = (3.14 * (radius * radius))
+r = int(input("enter the radius of the circle: "))
+area_circle = (3.14 * (r * r))
 print(f"the area of the circle is{area_circle}square meter.")
