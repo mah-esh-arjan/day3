8828b1743678174fc683afbe10cd2ec9a52ec0ce
MAHESH <mah.dot98@gmail.com>
2021-03-23 09:20:38 +0545

    d

---
+++ pythonworkshop/impo.py
@@ -0,0 +1,7 @@
+    
+def sum():
+    a=int(input("enter the first name:"))
+    b=int(input("enter the second name:"))
+    c=a+b
+#calling the function
+sum()
---
+++ pythonworkshop/sweet.py
@@ -0,0 +1,28 @@
+#defining function sum
+#defining function sum
+def sum():
+     num1=int(input("enter the first number"))
+     num2=int(input("ENter the second number: "))
+     answer=num1 + num2
+     print(f'The sum of {num1} and {num2} is {answer}')
+     print('funtcion end')
+
+#calling the function
+print("calling the funcion")
+sum()
+print('the program is over')
+def sub():
+    num1=int(input("enter the first number:"))
+    num2=int(input("enter the second number"))
+    answer=num1 - num2
+    print(f' the substraction of {num1} and {num2} is {answer}')
+    print('function end')
+sub()
+print('the program is over')
+def mult():
+    num1=int(input("enter the first number:"))
+    num2=int(input("enter the second number"))
+    answer=num1 * num2
+    print(f' the substraction of {num1} and {num2} is {answer}')
+    print('function end')
+mult()
