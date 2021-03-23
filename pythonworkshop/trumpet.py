64916da79dc24d1c98d879c923f23142d7fa41ac
MAHESH <mah.dot98@gmail.com>
2021-03-23 10:13:51 +0545

    d

---
+++ pythonworkshop/trumpet.py
@@ -0,0 +1,6 @@
+#check wether 5 is in the list of first natural numbers or not . hint {1,2,3,4}
+list=[1,2,3,4,5]
+if 5 in list:
+    print("there is 5 in list")
+else:
+    print("there is no 5 in list")
